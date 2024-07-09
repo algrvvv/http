8ebe369663750cf8c05a0b5f0d286165c3e02a20
p4xt3r <121817930+algrvvv@users.noreply.github.com>
2024-07-09 20:05:40 +0300

    создан простой логгер

---
+++ internal/logger/logger.go
@@ -0,0 +1,26 @@
+package logger
+
+import (
+	"fmt"
+	"os"
+)
+
+const (
+	SuccessLogType = 0
+	InfoLogType    = 1
+	ErrorLogType   = 2
+	ExitLogType    = 3
+)
+
+func Logger(log any, logType int) {
+	if logType == SuccessLogType {
+		fmt.Printf("[%s] - %v\n", Green("SUC"), log)
+	} else if logType == InfoLogType {
+		fmt.Printf("[%s] - %v\n", Blue("INFO"), log)
+	} else if logType == ErrorLogType {
+		fmt.Printf("[%s] - %v\n", Red("ERR"), log)
+	} else if logType == ExitLogType {
+		fmt.Printf("[%s] - %v\n", Red("EXIT"), log)
+		os.Exit(0)
+	}
+}
